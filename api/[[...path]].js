4009711305664d3fffeea7d4c3ed79e1f5883326
Jatin Kumar <jatin15011999@gmail.com>
2026-03-11 11:42:56 +0100

    deployment fixes - 2

--- api/[[...path]].js
+++ api/[[...path]].js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+  if (req.url && (req.url.startsWith('http://') || req.url.startsWith('https://'))) {
+    try {
+      const u = new URL(req.url);
+      req.url = u.pathname + (u.search || '');
+    } catch (_) {}
+  }
   const { default: app } = await import('../backend/server.js');
-  app(req, res);
+  return new Promise((resolve, reject) => {
+    res.on('finish', resolve);
+    res.on('error', reject);
+    app(req, res);
+  });
 }
